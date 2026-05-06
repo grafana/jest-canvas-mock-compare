01525a35b12d3b0ce029e7c75d9009a0ce261949
Galen <galen.kistler@grafana.com>
2026-05-06 12:14:27 -0500

    chore: remove whitespace change

--- packages/jest-canvas-mock-compare-viewer/server/dynamicPayloadMiddleware.test.ts
+++ packages/jest-canvas-mock-compare-viewer/server/dynamicPayloadMiddleware.test.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import os from 'node:os';
 import path from 'node:path';
+
 import { readListedPayloadMeta, resolveReadablePayloadUnderRoot } from './dynamicPayloadMiddleware.ts';
 
 describe('resolveReadablePayloadUnderRoot', () => {
